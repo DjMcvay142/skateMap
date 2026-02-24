74c0997a61e79f375d324999b56d6b7f3a5f948b
Dan McVay <danielmcvaymedia@gmail.com>
2026-02-24 00:36:20 +0000

    Implemented burger menu for mobile, added about page.

--- src/js/app.js
+++ src/js/app.js
@@ -70,3 +70,23 @@ findMeBtn.addEventListener("click", () => {
     },
   );
 });
+
+// Burger menu
+const burger = document.getElementById("burger");
+const navMenu = document.getElementById("nav-menu");
+const navOverlay = document.getElementById("nav-overlay");
+
+burger.addEventListener("click", () => {
+  navMenu.classList.toggle("open");
+  navOverlay.classList.toggle("open");
+});
+
+navOverlay.addEventListener("click", () => {
+  navMenu.classList.remove("open");
+  navOverlay.classList.remove("open");
+});
+
+document.getElementById("nav-close").addEventListener("click", () => {
+  navMenu.classList.remove("open");
+  navOverlay.classList.remove("open");
+});
